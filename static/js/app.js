bba44ec8adbe3a9f8fb5ce792a83bfc4a8b841a5
d2thef <dpflannery@gmail.com>
2019-01-03 23:45:37 -0500

    Added table for displaying level 1 people.
    I added a table and expanded my understanding of Vue.js'
    templating, for-loops, and AJAX handling.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,17 @@
-new Vue({
-      el: '#app',
+var people = new Vue({
+      el: '#people',
+      created() {
+          this.listPeople();
+      },
       data: {
-              message: 'Welcome party people!'
-            }
+          people: []
+     },
+     methods: {
+         listPeople(resource) {
+             this.$http.get('/people').then(response => {
+                     this.people = response.body.People;
+           }, response => {
+           });
+         }
+     }
 });
